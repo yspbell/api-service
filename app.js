493d2c7fb0dba3b56624f3de2d1b9f2f677ca990
rameezbell <rameez.charaniya@bell.ca>
2024-12-30 12:00:35 -0500

    added olympics router

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var animationsRouter = require('./routes/animations');
+var olympicsRouter = require('./routes/olympics');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/animations', animationsRouter);
+app.use('/olympics', olympicsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
