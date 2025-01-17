906fdfeb6242f7433da69893cf83237e58fa8520
rameezbell <rameez.charaniya@bell.ca>
2025-01-17 00:46:31 -0500

    updated splash animation url

--- routes/animations.js
+++ routes/animations.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 
-const animation = {"Events":[{"splashEventId":"7000","splashAnimationVersionTimeStamp":"2024-12-17","splashEventStartDate":"2024-12-17T09:00:00-05:00","splashEventEndDate":"2025-01-06T16:30:00-05:00","splashAnimationUrl":"/appassets/images/MBM/Animation/Boxing2024_en.json","splashAnimationDurationMills":"1000"}]};
+const animation = {"Events":[{"splashEventId":"7000","splashAnimationVersionTimeStamp":"2024-12-17","splashEventStartDate":"2024-12-17T09:00:00-05:00","splashEventEndDate":"2025-01-06T16:30:00-05:00","splashAnimationUrl":"https://assets.contentstack.io/v3/assets/blt8b7c282c86b582f7/bltcec9ac40eb0e3376/6787fe54ee0f423d18519ff5/Boxing2024_en.json","splashAnimationDurationMills":"1000"}]};
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send(200, animation);
